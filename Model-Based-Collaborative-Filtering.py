53fdae7ec87bd8b507089c297e76a5f1a0c58931
shariharan205 <shariharan205@gmail.com>
2018-09-28 00:34:26 -0700

    Reading data

--- Model-Based-Collaborative-Filtering.py
+++ Model-Based-Collaborative-Filtering.py
@@ -6,3 +6,10 @@ from sklearn.metrics import roc_curve, auc
 from surprise import accuracy, Reader, Dataset
 from surprise.model_selection import cross_validate, train_test_split, KFold
 from surprise.prediction_algorithms.matrix_factorization import SVD, NMF
+
+movies = pd.read_csv("ml-latest-small/movies.csv")
+ratings = pd.read_csv("ml-latest-small/ratings.csv")
+df = pd.DataFrame({'itemID': list(ratings.movieId), 'userID': list(ratings.userId), 'rating': list(ratings.rating)})
+
+reader = Reader(rating_scale=(1, 5))
+data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
