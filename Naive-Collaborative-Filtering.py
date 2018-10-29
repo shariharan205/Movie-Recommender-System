42325798ce37028ac77a52fd0d2db5653e698b41
shariharan205 <shariharan205@gmail.com>
2018-10-28 23:45:29 -0700

    Reading data

--- Naive-Collaborative-Filtering.py
+++ Naive-Collaborative-Filtering.py
@@ -14,3 +14,8 @@ from surprise.prediction_algorithms.matrix_factorization import SVD
 from surprise.prediction_algorithms.matrix_factorization import NMF
 from sklearn.metrics import mean_squared_error
 
+ratings = pd.read_csv('ratings.csv')
+movie = pd.read_csv('movies.csv')
+df = pd.DataFrame({'itemID': list(ratings.movieId), 'userID': list(ratings.userId), 'rating': list(ratings.rating)})
+reader = Reader(rating_scale=(0.5, 5.0))
+data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
