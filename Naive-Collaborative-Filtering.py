169974806490552d0474e31521a699bee95ed1f0
shariharan205 <shariharan205@gmail.com>
2018-10-31 23:31:10 -0700

    Naive filtering

--- Naive-Collaborative-Filtering.py
+++ Naive-Collaborative-Filtering.py
@@ -33,3 +33,13 @@ def unpopular_trim(data):
     movie_id_counter = Counter([val[1] for val in data])
     popular_trimmed_data = [val for val in data if movie_id_counter[val[1]] <= 2]
     return popular_trimmed_data
+
+
+def high_var_trim(data):
+    print("High variance trimming")
+    movie_rating_map = defaultdict(list)
+    for val in data:
+        movie_rating_map[val[1]].append(val[2])
+    high_var_data = [val for val in data if
+                     len(movie_rating_map[val[1]]) >= 5 and np.var(movie_rating_map[val[1]]) >= 2.0]
+    return high_var_data
