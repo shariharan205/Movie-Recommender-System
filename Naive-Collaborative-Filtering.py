045664d130788190156ac092db892b93ce2c73cb
shariharan205 <shariharan205@gmail.com>
2018-11-11 23:27:20 -0800

    Precision Recall at K

--- Naive-Collaborative-Filtering.py
+++ Naive-Collaborative-Filtering.py
@@ -157,3 +157,20 @@ algo = KNNWithMeans(k=20, sim_options={'name': 'pearson'})
 algo.fit(train_set)
 predictions = algo.test(test_set)
 top_recos = get_top_t(predictions)
+
+
+def precision_recall_at_k(predictions, k=10, threshold=3.5):
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
+                              for (est, true_r) in user_ratings[:k])
+        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 1
+        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 1
+    return precisions, recalls
